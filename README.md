babe62dc15389d12d05f8e0a71a9b01619c71c18
Samuel Schlesinger <samuel@simspace.com>
2020-01-22 15:27:27 -0500

    added a basic usage of a multi-producer and single consumer queue

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rust-Actor
+
+This is a "getting to know Rust" project which should allow a user to create an
+actor with the ability to then send it messages and have it reply to messages.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::mpsc::*;
+use std::sync::mpsc::{sync_channel};
+
+
+fn main() -> std::io::Result<()> {
+    let (sync_sender, receiver) = sync_channel(10000);
+    sync_sender.send(1).unwrap();
+    let msg = receiver.recv().unwrap();
+    println!("{:?}", msg);
+    Ok(())
 }
